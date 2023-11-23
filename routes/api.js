416097a37b1b5895e39f1fb664b5c97ebfd272d7
muflihanto <muflihanto@gmail.com>
2023-11-24 02:02:15 +0700

    add initial put method handler

--- routes/api.js
+++ routes/api.js
@@ -12,15 +12,18 @@ module.exports = function (app, myDataBase) {
 
     .get(function (req, res) {
       const fields = ["_id", "issue_title", "issue_text", "assigned_to", "status_text", "created_by", "open", "created_on", "updated_on"];
-      const queries = Object.fromEntries(Object.entries(req.query).filter(([key, value]) => fields.includes(key) && key !== "open" && value));
+      const queries = Object.fromEntries(Object.entries(req.query).filter(([key, value]) => fields.includes(key) && (key === "open" || (key !== "open" && value !== ""))));
 
       if (queries._id !== undefined) {
         queries._id = new ObjectId(queries._id);
       }
-      if (queries.open === "" || queries.open === "true") {
-        queries.open = true;
-      } else if (queries.open === "false") {
-        queries.open = false;
+      switch (queries.open) {
+        case "":
+        case "true":
+          queries.open = true;
+          break;
+        case "false":
+          queries.open = false;
       }
 
       const collection = myDataBase.collection(req.params.project);
@@ -64,7 +67,43 @@ module.exports = function (app, myDataBase) {
     })
 
     .put(function (req, res) {
-      let project = req.params.project;
+      if (req.body._id === undefined) return res.send({ error: "missing _id" });
+
+      const collection = myDataBase.collection(req.params.project);
+      let { _id = "", issue_title = "", issue_text = "", created_by = "", assigned_to = "", status_text = "", open = "" } = req.body;
+
+      switch (open) {
+        case "true":
+          open = true;
+          break;
+        case "false":
+          open = false;
+          break;
+        default:
+          open = "";
+      }
+
+      const updatedIssue = Object.fromEntries(Object.entries({ issue_title, issue_text, created_by, assigned_to, status_text, open }).filter(([_, value]) => value !== ""));
+
+      if (Object.keys(updatedIssue).length === 0) return res.send({ error: "no update field(s) sent", _id: _id });
+
+      try {
+        collection
+          .findOneAndUpdate(
+            { _id: new ObjectId(_id) },
+            {
+              $set: {
+                ...updatedIssue,
+                updated_on: new Date().toJSON(),
+              },
+            }
+          )
+          .then((doc) => {
+            res.send({ result: "successfully updated", _id: doc._id });
+          });
+      } catch {
+        res.send({ error: "could not update", _id: _id });
+      }
     })
 
     .delete(function (req, res) {
