d179346a861d16d357eff52c32982b68a54ef4f5
muflihanto <muflihanto@gmail.com>
2023-11-24 00:43:56 +0700

    add initial get method handler

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { ObjectId } = require("mongodb");
+
 /**
  * @param {import('express').Express} app - Express app instance
  * @param {import('mongodb').Db} myDataBase - Database instance
@@ -9,13 +11,34 @@ module.exports = function (app, myDataBase) {
     .route("/api/issues/:project")
 
     .get(function (req, res) {
-      let project = req.params.project;
+      const fields = ["_id", "issue_title", "issue_text", "assigned_to", "status_text", "created_by", "open", "created_on", "updated_on"];
+      const queries = Object.fromEntries(Object.entries(req.query).filter(([key, value]) => fields.includes(key) && key !== "open" && value));
+
+      if (queries._id !== undefined) {
+        queries._id = new ObjectId(queries._id);
+      }
+      if (queries.open === "" || queries.open === "true") {
+        queries.open = true;
+      } else if (queries.open === "false") {
+        queries.open = false;
+      }
+
+      const collection = myDataBase.collection(req.params.project);
+      collection
+        .find(queries)
+        .toArray()
+        .then((docs) => {
+          res.json(docs);
+        })
+        .catch((er) => {
+          res.send(er);
+        });
     })
 
     .post(function (req, res) {
       const collection = myDataBase.collection(req.params.project);
       const { issue_title, issue_text, assigned_to = "", status_text = "", created_by } = req.body;
-      const defaultTime = Date.now();
+      const defaultTime = new Date().toJSON();
 
       if (issue_title && issue_text && created_by) {
         const newIssue = {
@@ -36,7 +59,7 @@ module.exports = function (app, myDataBase) {
           })
           .catch((error) => res.send(error));
       } else {
-        res.send('Sorry, but "issue_title", "issue_text" and "created_by" are all required');
+        res.send({ error: "required field(s) missing" });
       }
     })
 
